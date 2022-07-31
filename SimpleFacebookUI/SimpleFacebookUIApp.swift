c80d5f32445fcc50fa47e861b5d47d0b3cc98a60
danixuz <danixuz@icloud.com>
2022-07-31 18:11:20 +0300

    Initial Commit

---
+++ SimpleFacebookUI/SimpleFacebookUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFacebookUIApp.swift
+//  SimpleFacebookUI
+//
+//  Created by Daniel Spalek on 31/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFacebookUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
